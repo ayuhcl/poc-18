36e59910c1ec4be20599a31f1b5e49204883fe7f
ayuhcl <ayushi-agarwal@hcltech.com>
2026-07-03 12:58:07 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,8 +1,8 @@
 output "ecr_repository_url" {
   value = aws_ecr_repository.website_repo.repository_url
 }
-output "aurora_endpoint" {
-  value = aws_rds_cluster.aurora.endpoint
+output "db_endpoint" {
+  value = aws_db_instance.postgres.address
 }
 output "alb_dns_name" {
   value = aws_lb.main.dns_name
