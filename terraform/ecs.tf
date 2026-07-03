a714fc01070e3aaac655a34676e5f78322c43709
ayuhcl <ayushi-agarwal@hcltech.com>
2026-07-03 12:05:21 +0530

    Update ecs.tf

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -27,3 +27,93 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+resource "aws_ecs_task_definition" "app" {
+  family                   = "poc18-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+
+  cpu    = "256"
+  memory = "512"
+
+  execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([
+    {
+      name  = "website"
+      image = "${aws_ecr_repository.website_repo.repository_url}:latest"
+
+      essential = true
+
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+          protocol      = "tcp"
+        }
+      ]
+
+      environment = [
+        {
+          name  = "DB_HOST"
+          value = aws_rds_cluster.aurora.endpoint
+        },
+        {
+          name  = "DB_USER"
+          value = "adminuser"
+        },
+        {
+          name  = "DB_PASSWORD"
+          value = "Password123!"
+        },
+        {
+          name  = "DB_NAME"
+          value = "mydb"
+        }
+      ]
+
+      logConfiguration = {
+        logDriver = "awslogs"
+
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.ecs_logs.name
+          awslogs-region        = var.aws_region
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+    }
+  ])
+}
+
+resource "aws_ecs_service" "app" {
+  name            = "poc18-service"
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.app.arn
+
+  desired_count = 2
+
+  launch_type = "FARGATE"
+
+  network_configuration {
+
+    subnets = [
+      aws_subnet.private_1.id,
+      aws_subnet.private_2.id
+    ]
+
+    security_groups = [
+      aws_security_group.ecs_sg.id
+    ]
+
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.main.arn
+    container_name   = "website"
+    container_port   = 3000
+  }
+
+  depends_on = [
+    aws_lb_listener.main
+  ]
+}
