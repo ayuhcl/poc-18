d9ac1d8da1e6e9fe4f529b47f21cf12000d9debb
ayuhcl <ayushi-agarwal@hcltech.com>
2026-07-03 12:00:32 +0530

    Create alb.tf

---
+++ terraform/alb.tf
@@ -0,0 +1,39 @@
+resource "aws_lb" "main" {
+  name               = "poc18-alb"
+  internal           = false
+  load_balancer_type = "application"
+
+  security_groups = [
+    aws_security_group.alb_sg.id
+  ]
+
+  subnets = [
+    aws_subnet.public_1.id,
+    aws_subnet.public_2.id
+  ]
+}
+
+resource "aws_lb_target_group" "main" {
+  name        = "poc18-tg"
+  port        = 3000
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"
+
+  health_check {
+    path = "/"
+  }
+}
+
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = aws_lb.main.arn
+
+  port     = 80
+  protocol = "HTTP"
+
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
+
