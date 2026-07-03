837440173393b075789f6f220df7fb3eaff423d0
ayuhcl <ayushi-agarwal@hcltech.com>
2026-07-03 14:26:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 const express = require('express');
-const app = express();
+const { Client } = require('pg');
 
-app.get('/', (req, res) => {
-    res.send(`
-        <h1>POC-18 AWS ECS Fargate Deployment</h1>
-        <h2>Application Running Successfully </h2>
-    `);
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const client = new Client({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: 5432
 });
 
-app.listen(3000, '0.0.0.0', () => {
-    console.log('Server running on port 3000');
+client.connect()
+    .then(() => console.log('Connected to PostgreSQL'))
+    .catch(err => console.error('Database connection error:', err));
+
+app.get('/', async (req, res) => {
+    try {
+        const result = await client.query('SELECT NOW()');
+
+        res.send(`
+            <h1>POC-18 AWS ECS Fargate + PostgreSQL</h1>
+            <h2>Database Connected Successfully </h2>
+            <p>Current DB Time: ${result.rows[0].now}</p>
+        `);
+    } catch (err) {
+        res.send(`Database Connection Error: ${err.message}`);
+    }
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
 });
